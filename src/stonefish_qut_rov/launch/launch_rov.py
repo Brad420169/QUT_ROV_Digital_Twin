44e193e943c53762dedc3089e7774b43ceccecc2
Brad Edwards <bradroy830@gmail.com>
2026-04-25 13:26:54 +1000

    Working Stonefish launch system with environment + robot include structure

--- src/stonefish_qut_rov/launch/launch_rov.py
+++ src/stonefish_qut_rov/launch/launch_rov.py
@@ -3,24 +3,24 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python import get_package_share_directory
 
-ENVIRONMENT = 'open_ocean'
+ENVIRONMENT = 'ocean_environment'
 
 def generate_launch_description():
-    launch_include = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            get_package_share_directory('stonefish_ros2') +
-            '/launch/stonefish_simulator.launch.py'
-        ),
-        launch_arguments={
-            'simulation_data': get_package_share_directory('stonefish_qut_rov'),
-            'scenario_desc': get_package_share_directory('stonefish_qut_rov') + f'/scenarios/{ENVIRONMENT}.scn',
-            'simulation_rate': '100.0',
-            'window_res_x': '1600',
-            'window_res_y': '1000',
-            'rendering_quality': 'high',
-        }.items()
-    )
+    pkg_dir = get_package_share_directory('stonefish_qut_rov')
 
     return LaunchDescription([
-        launch_include
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(
+                get_package_share_directory('stonefish_ros2') +
+                '/launch/stonefish_simulator.launch.py'
+            ),
+            launch_arguments={
+                'simulation_data': pkg_dir,
+                'scenario_desc': pkg_dir + f'/scenarios/{ENVIRONMENT}.scn',
+                'simulation_rate': '100.0',
+                'window_res_x': '1600',
+                'window_res_y': '1000',
+                'rendering_quality': 'high',
+            }.items()
+        )
     ])
