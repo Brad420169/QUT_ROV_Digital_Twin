793ccc5ef60cdae8115e11427352b552115650d0
Brad Edwards <bradroy830@gmail.com>
2026-04-25 20:58:47 +1000

    updating git on work done, tried defining a new rov model to better approximate buoyancy, and played around with some other files

--- src/stonefish_qut_rov/launch/launch_rov.py
+++ src/stonefish_qut_rov/launch/launch_rov.py
@@ -18,7 +18,7 @@ def generate_launch_description():
                 'simulation_data': pkg_dir,
                 'scenario_desc': pkg_dir + f'/scenarios/{ENVIRONMENT}.scn',
                 'simulation_rate': '100.0',
-                'window_res_x': '1600',
+                'window_res_x': '920',
                 'window_res_y': '1000',
                 'rendering_quality': 'high',
             }.items()
--- src/stonefish_qut_rov/ros_nodes/teleop.py
+++ src/stonefish_qut_rov/ros_nodes/teleop.py
@@ -1,59 +1,101 @@
 #!/usr/bin/env python3
+import sys
+import select
+import termios
+import tty
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float64MultiArray
-import sys, termios, tty
+
 
 class Teleop(Node):
     def __init__(self):
         super().__init__('teleop')
-        self.pub = self.create_publisher(Float64MultiArray, '/updatedqutrov/setpoint/pwm', 10)
+        self.pub = self.create_publisher(Float64MultiArray, '/qut_rov/setpoint/pwm', 10)
+        self.timer = self.create_timer(0.05, self.timer_callback)  # 20 Hz
 
-    def get_key(self):
-        fd = sys.stdin.fileno()
-        old = termios.tcgetattr(fd)
-        try:
-            tty.setraw(fd)
+        self.pwm = 500.0
+        self.thrust = [0.0, 0.0, 0.0, 0.0]
+
+        self.old_terminal_settings = termios.tcgetattr(sys.stdin)
+        tty.setcbreak(sys.stdin.fileno())
+
+        print("Controls (TOGGLE MODE):")
+        print("  w = forward")
+        print("  s = backward")
+        print("  a = up")
+        print("  d = down")
+        print("  space = stop")
+        print("  x = exit")
+        print("")
+        print("Press once to change command.")
+
+    def read_key_nonblocking(self):
+        if select.select([sys.stdin], [], [], 0.0)[0]:
             return sys.stdin.read(1)
-        finally:
-            termios.tcsetattr(fd, termios.TCSADRAIN, old)
+        return None
 
-    def run(self):
-        while True:
-            key = self.get_key()
-            print("KEY:", repr(key), flush=True)
+    def timer_callback(self):
+        key = self.read_key_nonblocking()
 
-            msg = Float64MultiArray()
-            thrust = [0.0, 0.0, 0.0, 0.0]
-
-            if key == 'i':
+        if key is not None:
+            if key == 'w':
+                self.thrust = [0.0, 0.0, self.pwm, self.pwm]
                 print("Forward", flush=True)
-                thrust = [1000, 1000, 1000, 1000]
 
-            elif key == 'k':
+            elif key == 's':
+                self.thrust = [0.0, 0.0, -self.pwm, -self.pwm]
                 print("Backward", flush=True)
-                thrust = [-1000, -1000, -1000, -1000]
 
-            elif key == 'u':
+            elif key == 'a':
+                self.thrust = [self.pwm, self.pwm, 0.0, 0.0]
                 print("Up", flush=True)
-                thrust = [1000, -1000, 0, 0]
 
-            elif key == 'j':
+            elif key == 'd':
+                self.thrust = [-self.pwm, -self.pwm, 0.0, 0.0]
                 print("Down", flush=True)
-                thrust = [-1000, 1000, 0, 0]
+
+            elif key == ' ':
+                self.thrust = [0.0, 0.0, 0.0, 0.0]
+                print("Stop", flush=True)
 
             elif key == 'x':
                 print("Exit", flush=True)
-                break
+                self.thrust = [0.0, 0.0, 0.0, 0.0]
+                self.publish_thrust()
+                self.cleanup()
+                rclpy.shutdown()
+                return
 
-            msg.data = thrust
-            self.pub.publish(msg)
+        # Always keep publishing current command
+        self.publish_thrust()
+
+    def publish_thrust(self):
+        msg = Float64MultiArray()
+        msg.data = self.thrust
+        self.pub.publish(msg)
+
+    def cleanup(self):
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, self.old_terminal_settings)
+
 
 def main():
     rclpy.init()
     node = Teleop()
-    node.run()
-    rclpy.shutdown()
+
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.thrust = [0.0, 0.0, 0.0, 0.0]
+        node.publish_thrust()
+        node.cleanup()
+        node.destroy_node()
+        if rclpy.ok():
+            rclpy.shutdown()
+
 
 if __name__ == '__main__':
     main()
